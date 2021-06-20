2f162227cc143dfa668a31c039eb3014823e8dc8
santhanamuthu <33146563+santhanamuthu@users.noreply.github.com>
2021-06-20 22:46:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return 'Hello, World! chandan'
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0",port=8080)
+	
